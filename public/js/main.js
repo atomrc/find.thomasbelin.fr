98545acd5d71ec8cac91a5366b6b577926456750
Thomas Belin <ThomasBelin4@gmail.com>
2013-03-12 23:01:53 +0100

    improve and use new version of phygine

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 requirejs.config({
     baseUrl: 'js/src',
     paths: {
-        app: ''
+        phygine: '../lib/phygine'
     }
 });
 
--- public/js/src/application.js
+++ public/js/src/application.js
@@ -1,37 +1,32 @@
 define(
-    ['physical-container', 'physical-element', 'vect', 'elastic', 'gravity', 'friction', 'requestanimationframe'],
-    function(PhysicalContainer, PhysicalElement, Vect, Elastic, Gravity, Friction, requestAnimationFrame) {
+    ['phygine'],
+    function(Phygine) {
         //the object containing all the application
         var application = {
-            container: null,
-            rAF:null,
             //launches the application
             init: function() {
-                this.rAF = requestAnimationFrame.bind(window);
                 var container = document.getElementById('physical-zone');
-                this.container = new PhysicalContainer(container);
-                this.container.addForce(new Gravity({acceleration: new Vect(0, 1)}));
-                this.container.addForce(new Friction({acceleration: new Vect(0.975, 0.975)}));
+                this.container = new Phygine.PhysicalContainer(container);
+                this.container.addForce(new Phygine.Gravity({acceleration: new Phygine.Vect(0, 1)}));
+                this.container.addForce(new Phygine.Friction({acceleration: new Phygine.Vect(0.975, 0.975)}));
                 var links = document.querySelectorAll('.link');
                 for(var i=0; i < links.length; i++) {
                     var link = links.item(i);
-                    var phyElement = new PhysicalElement(link, {x:4000, y:2500}),
+                    var phyElement = new Phygine.PhysicalElement(link, {x:4000, y:2500}),
                         elaX = parseInt(getComputedStyle(link, null).getPropertyValue('left')),
                         elaY = parseInt(getComputedStyle(link, null).getPropertyValue('top'));
                     var elasticOption = {
                         position: { x: elaX, y: elaY },
                         size: link.dataset.size
                     };
-                    var elastic = new Elastic(elasticOption);
+                    var elastic = new Phygine.Elastic(elasticOption);
                     phyElement.addForce(elastic);
                     this.container.add(phyElement);
                 }
             },
 
             run: function() {
-                this.container.update();
-                this.container.render();
-                this.rAF(this.run.bind(this));
+                this.container.run();
             }
         };
 
