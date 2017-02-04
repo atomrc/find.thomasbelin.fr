d09552bcdb1a741af24c44df9d68f16db68581ff
Thomas Belin <tom@doyoubuzz.com>
2017-02-04 13:02:47 +0100

    Change project organization

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  entry: './src/application.ts',
-  output: { path: __dirname + '/build', filename: 'application.js' },
+  entry: './src/js/application.ts',
+  output: { path: __dirname + '/dist/js', filename: 'application.js' },
   module: {
     loaders: [
       // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
